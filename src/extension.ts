a042a03dca3dba2f2f41ea4076b81642c40ab249
sinclairSt <stuartsinclair@rentalcars.com>
2020-06-02 17:44:05 +0100

    Removed a redundant import and fixed formatting in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,30 +4,30 @@ import { format } from './features/formatting/formatting';
 import { splitFile } from './features/splitChapters/split-chapters';
 import { subscribeToDocumentChanges } from './features/diagnostics/narration-diagnostics';
 import { DocumentSemanticTokensProvider, legend } from './features/highlighting/highlighting';
-import { DialogDecorator, applyDecorations } from './features/highlighting/DialogDecorator';
+import { applyDecorations } from './features/highlighting/DialogDecorator';
 
 export function activate(context: vscode.ExtensionContext) {
-	let currentFile = vscode.window.activeTextEditor.document.fileName;
-	let parentDir = path.dirname(currentFile);
+    let currentFile = vscode.window.activeTextEditor.document.fileName;
+    let parentDir = path.dirname(currentFile);
 
-	let splitChapters = vscode.commands.registerCommand('lit-code.splitChapters', () => {
-		splitFile(currentFile, parentDir);
-	});
-	
-	context.subscriptions.push(splitChapters);
+    let splitChapters = vscode.commands.registerCommand('lit-code.splitChapters', () => {
+        splitFile(currentFile, parentDir);
+    });
 
-	vscode.languages.registerDocumentFormattingEditProvider('markdown', {
-		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-			return format(document);
-		}
-	});
+    context.subscriptions.push(splitChapters);
 
-	const narrationDiagnostics = vscode.languages.createDiagnosticCollection("Lit-code: narration");
-	context.subscriptions.push(narrationDiagnostics);
+    vscode.languages.registerDocumentFormattingEditProvider('markdown', {
+        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+            return format(document);
+        }
+    });
+
+    const narrationDiagnostics = vscode.languages.createDiagnosticCollection("Lit-code: narration");
+    context.subscriptions.push(narrationDiagnostics);
 
     subscribeToDocumentChanges(context, narrationDiagnostics);
-    
-    context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'semanticLanguage'}, new DocumentSemanticTokensProvider(), legend));
+
+    context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'semanticLanguage' }, new DocumentSemanticTokensProvider(), legend));
 
     vscode.window.onDidChangeActiveTextEditor(editor => {
         applyDecorations();
